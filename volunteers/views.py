f258eb4581ca113b929b48f775ee4e3adc80dfe2
Clint Savage <herlo1@gmail.com>
2008-01-27 10:37:50 +0000

    added volunteer view

--- volunteers/views.py
+++ volunteers/views.py
@@ -1,10 +1,15 @@
 # Create your views here.
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse 
 from conman.common.models import Volunteer, UserProfile
+from conman.common.forms import VolunteerForm
 
-def index(request, ):
-    try:
-        v = Volunteer.objects.create(
-	vp = User.objects.get_profile(pk=user_id)
-    except User.DoesNotExist:
-        raise Http404
-    return render_to_response('volunteers.html')
+def index(request):
+    form = VolunteerForm(request.POST)
+    if form.is_valid():
+        v = Volunteers.objects.create(role=VolunteerRole.objects.create(name=form.name), 
+        request=VolunteerRole(name=form.requested_role), 
+        comments=form.comments)
+        HttpResponseRedirect(reverse('volunteer_accepted.html', args=(v.id)))
+    else:
+        return render_to_response('volunteer_template.html' )

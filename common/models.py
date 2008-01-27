4945b3b8994b7d7d3417ed040d59d16b75c24add
Kevin Kubasik <qub333@gmail.com>
2008-01-27 07:37:23 +0000

    changing models to be more configurable

--- common/models.py
+++ common/models.py
@@ -15,14 +15,6 @@ VOLUNTEER_CHOICES = (
     ('TEK', 'Technician'),
 )
 
-CATEGORY_CHOICES = (
-    ('RMGR', 'Room Manager'),
-    ('USH', 'Usher'),
-    ('GRT', 'Greeter'),
-    ('HRT', 'Heartsbane'),
-    ('TEK', 'Technician'),
-)
-
 SHIRT_SIZES = (
     ('S', 'Small'),
     ('XS', 'Extra-Small'),
@@ -36,14 +28,26 @@ SHIRT_SIZES = (
 class Category(models.Model):
     name = models.CharField(max_length=150,db_index=True)
 
+class VolunteerRoles(models.Model):
+    name = models.CharField(max_length=150, db_index=True)
+    
+class AudienceTypes(models.Model):
+    name = models.CharField(max_length=150, db_index=True)
+    
 class Volunteer(models.Model):
-    role = models.CharField(max_length=150, choices=VOLUNTEER_CHOICES,db_index=True)
-    request = models.CharField(max_length=150, choices=VOLUNTEER_CHOICES,db_index=True)
+    #role = models.CharField(max_length=150, choices=VOLUNTEER_CHOICES,db_index=True)
+    #request = models.CharField(max_length=150, choices=VOLUNTEER_CHOICES,db_index=True)
+    role = models.ForeignKey(VolunteerRoles)
+    request = models.ForeignKey(VolunteerRoles)
     comments = models.TextField()
     
+
+    
+    
 class Presentation(models.Model):
-    cat = models.CharField(max_length=200, choices=CATEGORY_CHOICES, db_index=True)
-    audience = models.CharField(max_length=200, choices=AUDIENCE_CHOICES,db_index=True)
+    cat = models.ForeignKey(Catagory)
+    audience = models.ForeignKey(AudienceTypes)
+   # audience = models.CharField(max_length=200, choices=AUDIENCE_CHOICES,db_index=True)
     shortsummary = models.CharField(max_length=500)
     longsummary = models.TextField()
     

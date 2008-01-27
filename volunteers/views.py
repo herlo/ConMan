b4701a71ac43efc457f486a20f5aac483a4de177
Clint Savage <herlo1@gmail.com>
2008-01-27 10:53:50 +0000

    volunteer postback views

--- volunteers/views.py
+++ volunteers/views.py
@@ -10,6 +10,10 @@ def index(request):
         v = Volunteers.objects.create(role=VolunteerRole.objects.create(name=form.name), 
         request=VolunteerRole(name=form.requested_role), 
         comments=form.comments)
-        HttpResponseRedirect(reverse('volunteer_accepted.html', args=(v.id)))
+        HttpResponseRedirect(reverse(conman.common.views.accepted, args=(v.id)))
     else:
-        return render_to_response('volunteer_template.html' )
+        return render_to_response('common/volunteer_template.html' )
+
+def accepted(request, v_id):
+    vol = get_object_or_404(Volunteer, pk=v_id)
+    return render_to_response('common/volunteer_accepted.html', {'vol_info': vol})

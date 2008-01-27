07ec72454cc3d3927e6c93bdb0cc9e172e5419fa
Kevin Kubasik <qub333@gmail.com>
2008-01-27 10:18:43 +0000

    adding volunteer template

--- speakers/views.py
+++ speakers/views.py
@@ -1,1 +1,9 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+from common.models import *
+from common.forms import *
+
+def index(request):
+    return render_to_response('call_for_papers.html',None)
+    
+def

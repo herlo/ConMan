90b7c2076530e7bb2a257eaa88484a9992a68ce1
Kevin Kubasik <qub333@gmail.com>
2008-01-27 09:17:15 +0000

    Initail Forms commit

--- common/forms.py
+++ common/forms.py
@@ -1,1 +1,22 @@
 from common.models import *
+from django import newforms as forms
+
+class VolunteerForm(forms.Form):
+    firstname = forms.CharField()
+    lastname = forms.CharField()
+    email = forms.EmailField()
+    title = forms.CharField()
+    role = forms.ChoiceField(role)
+    req = forms.CharField()
+    comments = forms.CharField()
+
+class PresenterForm(forms.Form):
+    firstname = forms.CharField()
+    lastname = forms.CharField()
+    email = forms.EmailField()
+    title = forms.CharField()
+    category = forms.ChoiceField(categories)
+    audience = forms.ChoiceField(audiences)
+    abstract = forms.CharField()
+    longabstract = forms.CharField()
+

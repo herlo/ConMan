097315a4c517fc81ca69d5c4dd6a78f7d018c925
Clint Savage <herlo1@gmail.com>
2008-01-27 13:05:03 +0000

    added fucking views for volunteers

--- volunteers/views.py
+++ volunteers/views.py
@@ -1,19 +1,21 @@
 # Create your views here.
 from django.http import HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse 
+from django.shortcuts import render_to_response
 from common.models import Volunteer, UserProfile, VolunteerRole
 from common.forms import VolunteerForm
-from django.shortcuts import render_to_response
 
 def index(request):
     form = VolunteerForm()
     if request.method == 'POST':
         form = VolunteerForm(request.POST)
         if form.is_valid():
-            v = Volunteer.objects.create(role=VolunteerRole.objects.create(name=form.data['name']), 
-            request=VolunteerRole(name=form.data['requested_role']), 
+            v = Volunteer.objects.create(role=VolunteerRole.objects.create(name=form.data['username']), 
+            request=VolunteerRole.objects.get(id=form.data['requested_role']), 
             comments=form.data['comments'])
-            HttpResponseRedirect(reverse(conman.common.views.accepted, args=(v.id)))
+
+            return render_to_response('volunteer_submitted.html')
+            #return HttpResponseRedirect(reverse('conman.common.views.submitted', args=(p.id,)))
         else:
             return render_to_response('call_for_volunteers.html', {'volunteers_form': form} )
     else:

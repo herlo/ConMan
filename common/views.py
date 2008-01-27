d5939ca94d0e91d570583278255f5b915719e923
Kevin Kubasik <qub333@gmail.com>
2008-01-27 06:55:17 +0000

    adding templates and inital views work

--- common/views.py
+++ common/views.py
@@ -1,1 +1,8 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+from common.models import *
+
+def index(request):
+    
+    return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
+

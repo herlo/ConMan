025408207916a1afe514dd3a4493b17c8bba656d
Kevin Kubasik <qub333@gmail.com>
2008-01-27 07:51:58 +0000

    fixing spelling error

--- common/models.py
+++ common/models.py
@@ -31,15 +31,24 @@ class Category(models.Model):
     def __str__(self):
         return name
     
+    class Admin:
+        pass
+
+    
 class VolunteerRoles(models.Model):
     name = models.CharField(max_length=150, db_index=True)
     def __str__(self):
         return name
+    class Admin:
+        pass
     
 class AudienceTypes(models.Model):
     name = models.CharField(max_length=150, db_index=True)
     def __str__(self):
         return name
+    class Admin:
+        pass
+    
     
 class Volunteer(models.Model):
     #role = models.CharField(max_length=150, choices=VOLUNTEER_CHOICES,db_index=True)
@@ -48,12 +57,17 @@ class Volunteer(models.Model):
     request = models.ForeignKey(VolunteerRoles)
     comments = models.TextField()
     
+    class Admin:
+        list_filter = ['role','request']
+        
 class Presentation(models.Model):
-    cat = models.ForeignKey(Catagory)
+    cat = models.ForeignKey(Category)
     audience = models.ForeignKey(AudienceTypes)
    # audience = models.CharField(max_length=200, choices=AUDIENCE_CHOICES,db_index=True)
     shortsummary = models.CharField(max_length=500)
     longsummary = models.TextField()
+    class Admin:
+        list_filter = ['cat','audience']
     
 class UserProfile(models.Model):
     user = models.ForeignKey(User)
@@ -61,3 +75,5 @@ class UserProfile(models.Model):
     presentation = models.ForeignKey(Presentation)
     shirtsize = models.CharField(max_length=200, db_index=True, choices=SHIRT_SIZES)
     volunteerinfo = models.ForeignKey(Volunteer)
+    class Admin:
+        pass

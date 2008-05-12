a53b1c07c7ecca16ceb67d6f997ff15f38e40072
Seth House <whiteinge@gmail.com>
2008-05-12 07:16:49 +0000

    Implemented voting on presentations for accounts with the appropriate permissions.

--- urls.py
+++ urls.py
@@ -1,12 +1,22 @@
 from django.contrib import databrowse
 from django.conf.urls.defaults import *
+from voting.views import vote_on_object
+from django.contrib.auth.decorators import user_passes_test
 from common.models import LatestEntries #, LatestEntriesByCategory
+from speakers.models import Presentation
 
 #feeds = {
 #    'latest': LatestEntries,
 #    'categories': LatestEntriesByCategory,
 #    'author': LatestEntriesByAuthor,
 #}
+
+presentation_dict = {
+    'model': Presentation,
+    'template_object_name': 'presentation',
+    'allow_xmlhttprequest': True,
+}
+can_vote = user_passes_test(lambda u: u.has_perm('can_vote'))
 
 urlpatterns = patterns('',
     (r'^admin/', include('django.contrib.admin.urls')),
@@ -20,8 +30,8 @@ urlpatterns = patterns('',
     (r'^speaker/list/$', 'speakers.views.show_speakers'),
     (r'^speaker/(?P<s_id>\d+)/$', 'speakers.views.speaker_info'),
     (r'^presentation/(?P<p_id>\d+)/$', 'speakers.views.show_presentation'),
+    (r'^presentation/(?P<object_id>\d+)/(?P<direction>up|down|clear)vote/?$', can_vote(vote_on_object), presentation_dict),
 #    (r'^speaker/$', 'speakers.views.index'),
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'static'}),
     (r'^$', 'common.views.index'),
 )
-

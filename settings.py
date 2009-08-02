131fc4774bb4023d2978800b49d8da5f91cb5d68
Clint Savage <herlo1@gmail.com>
2009-08-02 18:12:29 +0000

    altered the updates feature to better include social media and a more flexible template structure

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # Django settings for conman project.
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
--- updates/models.py
+++ updates/models.py
@@ -5,14 +5,16 @@ import settings
 if settings.TWITTER_ENABLED:
     import twitter
 
-# Create your models here.
 # Used to update what's going on before / during / after the conference.
-# could be used to update social networks too
+# Would like to enable more than twitter, but that's what we've got so far
 
 class Update(models.Model):
 
     name = models.CharField(max_length=150, db_index=True)
-    description = models.TextField(max_length=140,blank=True,null=True)
+    description = models.TextField(max_length=500,blank=True,null=True,help_text="No more than 500 characters")
+    link_title = models.CharField(max_length=25,blank=True,null=True,help_text="Something like 'Read More'")
+    link_url = models.CharField(max_length=150,blank=True,null=True,help_text="An existing url about this update")
+    social_info = models.CharField(max_length=140,blank=True,null=True,help_text="Tell the world something in 140 characters. Probably a short version of description")
     created = models.DateTimeField(editable=False)
     updated = models.DateTimeField(editable=False)
     author = models.ForeignKey(User)
@@ -22,9 +24,9 @@ class Update(models.Model):
             self.created = datetime.date.today()
             # add twitter code here (soon ping.fm)
             # we want it to succeed, but if it doesn't, oh well!
-            if settings.TWITTER_ENABLED:
+            if settings.TWITTER_ENABLED and self.social_info:
                 twit = twitter.Api(username=settings.TWITTER_USERNAME, password=settings.TWITTER_PASSWORD)
-                twit.PostUpdate(self.description)
+                twit.PostUpdate(self.social_info)
         self.updated = datetime.datetime.today()
         super(Update, self).save()
 

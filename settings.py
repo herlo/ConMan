e1c9bc55353e1534d16528723e6500163d2c7123
Adam Barrett <dragen@gmail.com>
2009-09-04 12:40:35 +0000

    style changes, cleaning things up

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ MANAGERS = (
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'testdatabase_trunk'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'root'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'testbed'         # Not used with sqlite3.
+DATABASE_PASSWORD = 'Dragen02'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 

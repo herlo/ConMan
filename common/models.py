eb1ebebb784726c525fc142cff7e83339b5c6078
Clint Savage <herlo1@gmail.com>
2010-01-24 22:16:14 -0700

    ading basic prep for theme based template loader

--- common/models.py
+++ common/models.py
@@ -33,11 +33,7 @@ class OverwriteStorage(FileSystemStorage):
         return name
 
 class ShirtSize(models.Model):
-    '''
-      >>> size = ShirtSize(name="2XL")
-      >>> size.get_name_display()
-      'XX Large'
-    '''
+
     name = models.CharField(max_length=50, choices=SHIRT_SIZES)
     def __unicode__(self):
         return self.get_name_display()
@@ -46,75 +42,6 @@ class ShirtSize(models.Model):
 #        list_display = ('id', 'name',)
 
 class UserProfile(models.Model):    
-    '''
-
-      >>> from django.contrib.auth.models import User
-
-    Create a User
-      >>> userMan = User.objects.create_user('Janx', 'lennon@thebeatles.com')
-
-    Create the category and audience for the presentation
-      >>> Category.objects.create(name="Crapology")
-      <Category: Crapology>
-      >>> AudienceType.objects.create(name="For Experts Only")
-      <AudienceType: For Experts Only>
-
-    Create a presentation for the user
-      >>> present = Presentation.objects.create(
-      ... cat=Category.objects.get(id=1),
-      ... audience=AudienceType.objects.get(id=1),
-      ... abstract="crapology in a nutshell",
-      ... longabstract="To enlighten on the subject of crap"
-      ... )
-
-    Create a shirtSize for the user
-      >>> shirt = ShirtSize.objects.create(name="XXXXS")
-
-    Create a VolunteerRole for the volunteer
-      >>> VolunteerRole.objects.create(name="Fun Sucker")
-      <VolunteerRole: Fun Sucker>
-
-    Create a volunteer with the user
-
-      >>> vol = Volunteer.objects.create(
-      ... role=VolunteerRole.objects.get(id=1),
-      ... request=VolunteerRole.objects.get(id=1),
-      ... comments="This is my comment")
-
-      >>> userProfile = UserProfile(
-      ... user=userMan,
-      ... bio="I am the best in my field",
-      ... presentation=present,
-      ... shirtsize=shirt,
-      ... volunteerinfo=vol,
-      ... job_title="The master of the universe of crap",
-      ... irc_nick="CrapMaster",
-      ... irc_server="FreeNode",
-      ... common_channels="#crapology, #shitonomics")
-
-
-      >>> userProfile
-      <UserProfile: Janx's profile>
-      >>> userProfile.user
-      <User: Janx>
-      >>> userProfile.bio
-      'I am the best in my field'
-      >>> userProfile.presentation
-      <Presentation:  Presentation 2>
-      >>> userProfile.shirtsize
-      <ShirtSize: XXXXS>
-      >>> userProfile.volunteerinfo
-      <Volunteer: Fun Sucker Volunteer 1>
-      >>> userProfile.job_title
-      'The master of the universe of crap'
-      >>> userProfile.irc_nick
-      'CrapMaster'
-      >>> userProfile.irc_server
-      'FreeNode'
-      >>> userProfile.common_channels
-      '#crapology, #shitonomics'
-
-    '''
 
 #    volunteerinfo = models.ForeignKey(Volunteer, null=True, blank=True,edit_inline=models.STACKED,num_extra_on_change=1)
     #presentation = models.ForeignKey(Presentation, null=True, blank=True,)
@@ -133,28 +60,13 @@ class UserProfile(models.Model):
     def __unicode__(self):
         return self.user.first_name + ' ' + self.user.last_name
 
-#    class Admin:
-#        search_fields = ['job_title','common_channels','@bio','site']
-#        list_display = ('user', 'irc_nick', 'common_channels')
-#        fields = (
-#            (None, {
-#                'fields': ('user', 'bio', 'job_title', 'site')
-#            }),
-#            ('Presentations', {
-#                'classes': 'collapse',
-#                'fields' : ('presentation')
-#            }),
-#            ('Volunteer Info', {
-#                'classes': 'collapse',
-#                'fields' : ('volunteer_info')
-#            }),
-#            ('Photo', {
-#                'classes': 'collapse',
-#                'fields' : ('user_photo')
-#            }),
-#            ('Misc Info', {
-#                'classes': 'collapse',
-#                'fields' : ('shirt_size', 'irc_nick', 'irc_server', 'common_channels')
-#            }),
-#        )
+
+class Theme(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+    description = models.TextField(blank=True)
+    slug = models.SlugField(max_length=50, unique=True, editable=False)
+    preview = models.ImageField(blank=True,null=True,upload_to='theme_preview/')
+
+    def __unicode__(self):
+        return '%s' % self.name
 

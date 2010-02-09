f8c24b88e639745f7fa477fb1443185768f222b9
Clint Savage <herlo1@gmail.com>
2010-02-08 19:31:13 -0700

    added a comment regarding the DJANGO_SETTINGS_MODULE environment variable

--- settings.py
+++ settings.py
@@ -1,3 +1,9 @@
 from global_settings import *
 # don't change this file
 # use local_settings for any local changes you may need
+#
+# to use this configuration, the DJANGO_SETTINGS_MODULE=local_settings must be used
+# The easiest way to do that is to create a script or something that does the following:
+#
+# export DJANGO_SETTINGS_MODULE=local_settings
+

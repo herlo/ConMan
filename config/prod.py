de859fc84469e6c8f47c1b7efe64c36c39195e82
Kevin Carter <cartkev@gmail.com>
2010-02-10 15:37:47 -0700

    Use full path in settings

--- config/prod.py
+++ config/prod.py
@@ -1,3 +1,5 @@
+import os
+from settings import SITE_ROOT
 # Django settings for conman project.
 
 DEBUG = False
@@ -39,7 +41,7 @@ USE_I18N = False
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = 'public/'
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'public/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -82,7 +84,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "templates",
+    os.path.join(SITE_ROOT, "templates"),
 )
 
 INSTALLED_APPS = (
@@ -110,16 +112,16 @@ INSTALLED_APPS = (
     # tar xf /tmp/sorl-thumbnail.tar.gz -C /tmp && cd /tmp/sorl-thumbnail-3.2.5/ && python setup.py install
     'sorl.thumbnail',
     #useful migrations tool -- http://south.aeracode.org/
-    #'south',
+    'south',
 )
 
 ###########NEW CHANGE TO ADD###########
 TEMPLATE_CONTEXT_PROCESSORS = (
-'django.core.context_processors.auth',
-'django.core.context_processors.debug',
-'django.core.context_processors.i18n',
-'django.core.context_processors.media',
-'django.core.context_processors.request'
+    'django.core.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.request'
 )
 ######################################
 
@@ -143,7 +145,7 @@ EMAIL_USE_TLS = True
 EMAIL_SUBJECT_PREFIX = '[conman-papers] '
 SERVER_EMAIL = 'admin@utos.org'
 
-FONT_PATH = "public/fonts/DejaVuLGCSerifCondensed.ttf"
+FONT_PATH = os.path.join(SITE_ROOT, "public/fonts/DejaVuLGCSerifCondensed.ttf")
 #FONT_PATH="arial.pil"
 FONT_SIZE = 14
 # Timeout in minutes

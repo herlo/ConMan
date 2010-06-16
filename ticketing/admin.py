a68e576f36c404f14f434bede6169e56c6f76827
Clint Savage <herlo1@gmail.com>
2010-06-15 21:11:14 -0600

    removing AdminSite as it's already been declared elsewhere

--- ticketing/admin.py
+++ ticketing/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from django.contrib.sites.models import Site
 from ticketing.models import TicketType,Ticket,Discount,Item
 
-site = admin.AdminSite()
-
 #ticketing
 admin.site.register(Item)
 admin.site.register(Discount)
